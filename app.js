7a53d1a149ac84bca35bf50e4ba5600c9a722339
kacho65535 <shunnishida25251204@gmail.com>
2020-03-05 17:33:05 +0900

    取り簿なっち

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 'use strict';
-const length = 100;
+const length = 100000;
 var vec = Array(length);
 vec.fill(0);
 function fib(n) {
-    if (n <= 1) return 1;
-    else {
+    if (n <= 1) return 0;
+    if (n == 2) return 1;
+    if (n > 2) {
         if (vec[n] != 0) return vec[n];
-        else return vec[n] = fib(n - 1) + fib(n - 2);
+        else return vec[n] = fib(n - 1) + fib(n - 2) + fib(n - 3);
     }
 }
 for (let i = 0; i < length; i++) {

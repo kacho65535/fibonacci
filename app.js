af75a5707aceb69d8c8f7491e17288b66da7ca49
kacho65535 <shunnishida25251204@gmail.com>
2020-03-05 17:12:00 +0900

    メモ化再帰の実装(もうした)

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 'use strict';
+const length = 100;
+var vec = Array(length);
+vec.fill(0);
+function fib(n) {
+    if (n <= 1) return 1;
+    else {
+        if (vec[n] != 0) return vec[n];
+        else return vec[n] = fib(n - 1) + fib(n - 2);
+    }
+}
+for (let i = 0; i < length; i++) {
+    console.log(fib(i));
+}
